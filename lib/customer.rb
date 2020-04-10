f0b05e09cd36d1b4b6de9a05961cba822e662ec9
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-10 22:27:05 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -8,13 +8,19 @@ class Customer
     @@all << self
   end
 
-  def new_meal(waiter, tota, tip=0)#Creating the Has-Many-Through relationship
+  def new_meal(waiter, total, tip=0)#Creating the Has-Many-Through relationship
     Meal.new(waiter, self, total, tip)
   end
 
   def meals
-    Meal.all.select do |meal|
+    Meal.all.select do |meal| #seeing the meals they have eaten at the resturant
       meal.customer = self
+    end
+  end
+
+  def waiters #seeing the waiters that served them in the past
+    meals.map do |meal|
+      meal.waiter
     end
   end
 
